87ba822f690fccc8df1967e1d1385c0d8a93090e
mackarpov <mackarpov@gmail.com>
2025-06-10 00:33:42 +0300

    KS-1-3-space solving

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,20 @@
+package ru.ievetrov.lesson_1
+
+import kotlin.math.min
+
+fun main() {
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+
+    print(hour)
+    print(":")
+    print(minute)
+}
