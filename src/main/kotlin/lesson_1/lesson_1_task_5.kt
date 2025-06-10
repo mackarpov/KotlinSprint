9db30c056320b3471c6fe9b3955dedbf9b758ff8
mackarpov <mackarpov@gmail.com>
2025-06-10 21:07:07 +0300

    KS-1-5 space3 solving, work on bugs

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,11 +1,13 @@
 package ru.ievetrov.lesson_1
 
 fun main() {
+    val secondsInMinute: Byte = 60
+    val minutesInHour: Byte = 60
     val timeInSeconds = 6_480
-    val timeInMinutes = timeInSeconds / 60
-    val timeInHours = timeInMinutes / 60
-    val remainingTimeInSeconds = timeInSeconds % 60
-    val remainingTimeInMinutes = timeInMinutes % 60
+    val timeInMinutes = timeInSeconds / secondsInMinute
+    val timeInHours = timeInMinutes / minutesInHour
+    val remainingTimeInSeconds = timeInSeconds % secondsInMinute
+    val remainingTimeInMinutes = timeInMinutes % minutesInHour
 
     println("%02d:%02d:%02d".format(timeInHours, remainingTimeInMinutes, remainingTimeInSeconds))
 }
