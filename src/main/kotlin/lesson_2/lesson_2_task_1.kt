8869a2fdee89f73ed5bf9419f1f4b1d89839c652
mackarpov <mackarpov@gmail.com>
2025-06-11 18:27:02 +0300

    KS-2-1 average mark

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,12 @@
+package ru.ievetrov.lesson_2
+
+fun main() {
+    val markFirstStudent: Byte = 3
+    val markSecondStudent: Byte = 4
+    val markThirdStudent: Byte = 3
+    val markFourthStudent: Byte = 5
+    val sumOfMarks = markFirstStudent + markSecondStudent + markThirdStudent + markFourthStudent
+    val averageMark = (sumOfMarks / 4).toDouble()
+
+    println("%,.2f".format(averageMark))
+}
