c4fcac91fc5a7da92e6a820297a0c8d9fbc62590
mackarpov <mackarpov@gmail.com>
2025-06-12 15:35:46 +0300

    KS-2-4 change rate of buff

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -3,10 +3,10 @@ package ru.ievetrov.lesson_2
 fun main() {
     val cristalOre: Int = 7
     val metalOre: Int = 11
-    val buff = 1.2
+    val buff = 20.0
 
-    val cristalOreBuff = cristalOre * buff
-    val metalOreBuff = metalOre * buff
+    val cristalOreBuff = cristalOre * (buff / 100 + 1)
+    val metalOreBuff = metalOre * (buff / 100 + 1)
 
     println(cristalOreBuff.toInt())
     println(metalOreBuff.toInt())
